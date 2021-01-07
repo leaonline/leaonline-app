e06dd8709a1cc7863b111cff735ad7d1de1708c7
aakalaew <arthur.akalaew@web.de>
2021-01-07 08:57:28 -0800

    added emulator script to prepare environment

--- scripts/android/build_android_apk.sh
+++ scripts/android/build_android_apk.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-meteor build android
+METEOR_PROJECT_PATH=$(pwd)
+SERVER="${APPLICATION_NAME}.meteor.com"
+APPLICATION_NAME="$(basename "$METEOR_PROJECT_PATH")_apk"
+
+cd "${METEOR_PROJECT_PATH}"/../.. || exit
+
+
+meteor add-platform android
+meteor build "${APPLICATION_NAME}" --server="${SERVER}"
+
+
