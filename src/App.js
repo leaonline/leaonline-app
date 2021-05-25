7234c2dd5157aff9547941911a8a2c6955624c4e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-05-25 07:59:17 -0400

    basic navigation between Home and About

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Button, StyleSheet, Text, View, Alert } from 'react-native'
 import { Header } from 'react-native-elements'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+
+const Stack = createStackNavigator();
 // import Meteor, { Mongo, withTracker } from '@meteorrn/core'
 // import Meteor from '@meteorrn/core'
 
 // Meteor.connect("ws://127.0.0.1:3000/websocket");
 
-export default function App () {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text testID='textField'>Test Meteor Application
-
-        <Header
-          leftComponent={{ icon: 'menu', color: '#fff' }}
-          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'home', color: '#fff' }}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
         />
-      </Text>
-      <StatusBar style='auto' />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+        />
+     </Stack.Navigator>
+     {/*  <View style={styles.header}>
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'lea.online Application ', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+          />
+          <View style={styles.container}>
 
-      <Button
-        testID='Button'
-        title='Press This Button, please'
-        onPress={() => Alert.alert('Button is working')}
-      />
-    </View>
+            <Text testID='textField'>Test Meteor Application
+            </Text>
+            <StatusBar style='auto' />
 
+            <Button
+              testID='Button'
+              title='Press This Button, please'
+              onPress={() => Alert.alert('Button is working')}
+            />
+          </View>
+        </View> */}
+    </NavigationContainer>
+    
   )
 }
 
@@ -36,5 +57,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+
+  header: {
+    width: '100%',
+    height: '100%'
+
   }
 })
