a0068c6f6af4f2223577d3e8ecd3314ccf82e325
Muhammed Yildiz <imalifire309@gmail.com>
2021-06-27 07:57:19 -0400

    added Colors, prototype apploader/fetchFonts

---
+++ src/constants/Colors.js
@@ -0,0 +1,11 @@
+export default {
+    primary: '#f59d1d',
+    secondary: '#193b5e',
+    light: '#f5f5f5',
+    gray: '#a6a6a6',
+    dark: '#646464',
+    warning: '#FFC800',
+    danger: '#d95a7d',
+    success: '#00ddb6',
+    info: '#86c4f1'
+}
