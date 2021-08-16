e85ce32cf1b9d31f63367ab462db801b21b9b637
Muhammed Yildiz <imalifire309@gmail.com>
2021-08-16 05:59:38 -0400

    refactored __tests__, added TitleText-Tests in tts.test

--- src/components/TitleText.js
+++ src/components/TitleText.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Text, StyleSheet } from 'react-native'
 
+/**
+ * TitleText is a component of Tts. It displays the spoken text.
+ * @param {string} props.text: The displayed text
+ * @param {css} props.style: The style elements for the text
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const TitleText = props => <Text style={{ ...styles.body, ...props.style }}>{props.text}</Text>
 
 const styles = StyleSheet.create({
