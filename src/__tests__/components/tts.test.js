3df0fe18d2ba30779412c57f2be9339d564a3484
Jan Küster <jkuester@uni-bremen.de>
2021-08-27 11:19:15 +0200

    tts tests fixed

--- src/__tests__/components/tts.test.js
+++ src/__tests__/components/tts.test.js
@@ -3,52 +3,68 @@ import renderer from 'react-test-renderer'
 import TitleText from '../../components/TitleText'
 import Colors from '../../constants/Colors'
 import WelcomeScreen from '../../screens/WelcomeScreen'
-import Tts from '../../components/Tts'
+import { TTSengine } from '../../components/Tts'
 import { fireEvent, render, waitFor } from '@testing-library/react-native'
-import { act } from 'react-dom/test-utils';
-
-jest.mock('expo-speech', () => {
-  return () => ({
-    maxSpeechInputLength: jest.fn(),
-    isSpeakingAsync: jest.fn(),
-  })
-})
+import { act } from 'react-dom/test-utils'
 
 test('Text alignment left', () => {
-  const tree = renderer.create(<TitleText style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'left' }}
-                                          text={'Hallo'}/>)
+  const tree = renderer.create(<TitleText
+    style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'left' }}
+    text={'Hallo'}/>)
   expect(tree).toMatchSnapshot()
 })
 
 test('Text alignment right', () => {
-  const tree = renderer.create(<TitleText style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'right' }}
-                                          text={'Hallo'}/>)
+  const tree = renderer.create(<TitleText
+    style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'right' }}
+    text={'Hallo'}/>)
   expect(tree).toMatchSnapshot()
 })
 
 test('Text alignment center', () => {
-  const tree = renderer.create(<TitleText style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'center' }}
-                                          text={'Hallo'}/>)
+  const tree = renderer.create(<TitleText
+    style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'center' }}
+    text={'Hallo'}/>)
   expect(tree).toMatchSnapshot()
 })
 
 test('Text color secondary', () => {
-  const tree = renderer.create(<TitleText style={{ color: Colors.secondary, paddingLeft: 5, textAlign: 'center' }}
-                                          text={'Hallo'}/>)
+  const tree = renderer.create(<TitleText
+    style={{ color: Colors.secondary, paddingLeft: 5, textAlign: 'center' }}
+    text={'Hallo'}/>)
   expect(tree).toMatchSnapshot()
 })
 
 it('should find the button via testId', () => {
-  const {getByTestId} = render(<WelcomeScreen />);
-
-  const foundButton = getByTestId('welcomeScreen1');
-  expect(foundButton).toBeTruthy();
-});
+  const { getByTestId } = render(<WelcomeScreen/>)
+  const foundButton = getByTestId('welcomeScreen1')
+  expect(foundButton).toBeTruthy()
+})
 
 it('tts (asynch) test', async () => {
-  const {getByTestId} = render(<WelcomeScreen />);
+  let speakCalled = false
+  let stopCalled = false
 
-  const foundButton = getByTestId('welcomeScreen1');
-  await fireEvent.press(foundButton);
-  await waitFor(() => expect(global.ttsIsCurrentlyPlaying).toBeTruthy());
-});
+  TTSengine.setSpeech({
+    isSpeakingAsync: async function () {
+      return false
+    },
+    speak: t => {
+      expect(t).toBe('Herzlich Willkommen zu lea online')
+      global.ttsIsCurrentlyPlaying = true
+      speakCalled = true
+    },
+    stop: () => {
+      stopCalled = true
+    }
+  })
+
+  const { getByTestId } = render(<WelcomeScreen/>)
+  const foundButton = getByTestId('welcomeScreen1')
+  await fireEvent.press(foundButton)
+  await waitFor(() => {
+    expect(global.ttsIsCurrentlyPlaying).toBeTruthy()
+    expect(speakCalled).toBe(true)
+    expect(stopCalled).toBe(false)
+  })
+})
