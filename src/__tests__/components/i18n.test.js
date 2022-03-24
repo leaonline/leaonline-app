84f4a14948e281bcc1d7a86b785e3d600c915c7b
Muhammed Ali Yildiz <maliyildiz@outlook.de>
2022-03-24 08:40:10 -0400

    changed i18n test

--- src/__tests__/components/i18n.test.js
+++ src/__tests__/components/i18n.test.js
@@ -3,13 +3,33 @@ import i18n from '../../i18n'
 const translationEN = i18n.getDataByLanguage('en')
 const translationDE = i18n.getDataByLanguage('de')
 
-test('check if the same namespaces exists in DE and EN', () => {
-  expect(Object.keys(translationEN)).toEqual(Object.keys(translationDE))
-})
+test('recursively iterate all object keys of i18 EN and DE, checks if the same namespaces exists, if namespaces have the same length', () => {
+  const isPlainObj = obj => Object.prototype.toString.call(obj) === '[object Object]'
+  const equal = (a, b) => {
+    const keysa = Object.keys(a).sort()
+    const keysb = Object.keys(b).sort()
 
-test('check if the namespaces in DE and EN have the same values', () => {
-  const valuesEN = Object.keys(translationEN).map(key => Object.keys(i18n.getResourceBundle('en', key)))
-  const valuesDE = Object.keys(translationDE).map(key => Object.keys(i18n.getResourceBundle('de', key)))
+    if (keysa.length !== keysb.length) {
+      return false
+    }
 
-  expect(valuesDE).toEqual(valuesEN)
+    if (keysa.toString() !== keysb.toString()) {
+      return false
+    }
+
+    return keysa.every(key => {
+      const valuea = a[key]
+      const valueb = b[key]
+      const typea = typeof valuea
+      const typeb = typeof valueb
+
+      if (typea !== typeb) { return false }
+      if (typea === 'string') { return true }
+      if (!isPlainObj(valuea) || !isPlainObj(valueb)) { return false }
+
+      return equal(valuea, valueb)
+    })
+  }
+
+  expect(equal(translationEN, translationDE)).toBe(true)
 })
