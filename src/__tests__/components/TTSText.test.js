c9994c030eae2b22aa447db67b26c3577c4111f0
jankapunkt <jkuester@uni-bremen.de>
2022-08-16 18:50:42 +0200

    fix(tests): fix remaining lint errors

--- src/__tests__/components/TTSText.test.js
+++ src/__tests__/components/TTSText.test.js
@@ -6,27 +6,31 @@ import Colors from '../../constants/Colors'
 test('Text alignment left', () => {
   const tree = renderer.create(<TTSText
     style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'left' }}
-    text='Hallo' />)
+    text='Hallo'
+                               />)
   expect(tree).toMatchSnapshot()
 })
 
 test('Text alignment right', () => {
   const tree = renderer.create(<TTSText
     style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'right' }}
-    text='Hallo' />)
+    text='Hallo'
+                               />)
   expect(tree).toMatchSnapshot()
 })
 
 test('Text alignment center', () => {
   const tree = renderer.create(<TTSText
     style={{ color: Colors.primary, paddingLeft: 5, textAlign: 'center' }}
-    text='Hallo' />)
+    text='Hallo'
+                               />)
   expect(tree).toMatchSnapshot()
 })
 
 test('Text color secondary', () => {
   const tree = renderer.create(<TTSText
     style={{ color: Colors.secondary, paddingLeft: 5, textAlign: 'center' }}
-    text='Hallo' />)
+    text='Hallo'
+                               />)
   expect(tree).toMatchSnapshot()
 })
