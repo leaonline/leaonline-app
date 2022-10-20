10abb3446dd60908b1e052e32a61e34cccc53c47
jankapunkt <jkuester@uni-bremen.de>
2022-10-20 07:14:40 +0200

    fix(app): ErrorMessage component renders null on empty error

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -2,14 +2,37 @@ import React from 'react'
 import { TTSengine } from './Tts'
 import { ActionButton } from './ActionButton'
 import { View } from 'react-native'
+import { createStyleSheet } from '../styles/createStyleSheet'
+import Colors from '../constants/Colors'
+import { Layout } from '../constants/Layout'
 
 const Tts = TTSengine.component()
 
 export const ErrorMessage = ({ error, message, label, onConfirm }) => {
-  return (
-    <View>
-      <Tts text={message || error.message} />
+  if (!error && !message) {
+    return null
+  }
+
+  const renderConfirm = () => {
+    if (!onConfirm) { return null }
+    return (
       <ActionButton text={label} onPress={onConfirm} />
+    )
+  }
+  return (
+    <View style={styles.default}>
+      <Tts iconColor={Colors.danger} color={Colors.danger} text={message || error.message} />
+      {renderConfirm()}
     </View>
   )
 }
+
+const styles = createStyleSheet({
+  default: {
+    borderWidth: 0.5,
+    padding: 10,
+    borderColor: Colors.danger,
+    backgroundColor: Colors.white,
+    ...Layout.dropShadow()
+  }
+})
