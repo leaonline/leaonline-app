1c6103df81f52630f8a092c8a8497b615d25bffc
jankapunkt <jkuester@uni-bremen.de>
2022-10-20 07:19:44 +0200

    feature(app): useLogin includes signUp/restore/signOut

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -9,23 +9,23 @@ const initialState = {
 }
 
 /** @private */
-const reducer = (state, action) => {
-  switch (action.type) {
+const reducer = (prevState, nextState) => {
+  switch (nextState.type) {
     case 'RESTORE_TOKEN':
       return {
-        ...state,
-        userToken: action.token,
+        ...prevState,
+        userToken: nextState.token,
         isLoading: false
       }
     case 'SIGN_IN':
       return {
-        ...state,
+        ...prevState,
         isSignOut: false,
-        userToken: action.token
+        userToken: nextState.token
       }
     case 'SIGN_OUT':
       return {
-        ...state,
+        ...prevState,
         isSignout: true,
         userToken: null
       }
@@ -66,28 +66,66 @@ export const useLogin = () => {
   // MeteorRN loads the token on connection automatically,
   // in case it exists, but we need to "know" that for our auth workflow
   useEffect(() => {
-    const handleOnLogin = () => dispatch({ type: 'RESTORE_TOKEN', token: Meteor.getAuthToken() })
-    Data.on('onLogin', handleOnLogin)
-    return () => Data.off('onLogin', handleOnLogin)
+    const authToken = Meteor.getAuthToken()
+
+    // There may be the situation where the auth token
+    // is immediately available. If so, we can safely
+    // dispatch and don't need any further information.
+    if (authToken) {
+      dispatch({ type: 'RESTORE_TOKEN', token: Meteor.getAuthToken() })
+    }
+
+      // Otherwise, we need to listen to the package's
+      // DDP even 'onLogin'  and dispatch then.
+      // Note, that 'onLogin'  is only fired, when the
+    // package has successfully restored the login via token!
+    else {
+      const handleOnLogin = () => dispatch({ type: 'RESTORE_TOKEN', token: Meteor.getAuthToken() })
+      Data.on('onLogin', handleOnLogin)
+      return () => Data.off('onLogin', handleOnLogin)
+    }
   }, [])
 
   // the auth can be referenced via useContext in the several
   // screens later on
   const authContext = useMemo(() => ({
-    signIn: ({ codes, onError }) => {
-      
-    },
-    signOut: () => {
+    signOut: ({ onError }) => {
       Meteor.logout(err => {
-        if (err) {
-          // TODO display error, merge into the above workflow
-          return console.error(err)
-        }
+        if (err) { return onError(err) }
         dispatch({ type: 'SIGN_OUT' })
       })
     },
-    signUp: ({ onError }) => {
+    signUp: ({ voice, speed, onError }) => {
+      Meteor.call('users.methods.create', { voice, speed }, (err, res) => {
+        if (err) {
+          return onError(err)
+        }
 
+        // the package handles the token result for us
+        // and uses our defined storage to store the token
+        // in order to use it for auto-login when we open
+        // the app next time
+        Meteor._handleLoginCallback(err, res)
+        const token = Meteor.getAuthToken()
+        const type = 'SIGN_IN'
+        dispatch({ type, token })
+      })
+    },
+    restore: ({ codes, voice, speed, onError }) => {
+      Meteor.call('users.methods.restore', { codes, voice, speed }, (err, res) => {
+        if (err) {
+          return onError(err)
+        }
+
+        // the package handles the token result for us
+        // and uses our defined storage to store the token
+        // in order to use it for auto-login when we open
+        // the app next time
+        Meteor._handleLoginCallback(err, res)
+        const token = Meteor.getAuthToken()
+        const type = 'SIGN_IN'
+        dispatch({ type, token })
+      })
     }
   }), [])
 
