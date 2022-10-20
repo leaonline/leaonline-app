86448938e212da2da4cd01ab0af67201b35a81aa
jankapunkt <jkuester@uni-bremen.de>
2022-10-20 07:19:59 +0200

    add(app): useUser hook added

---
+++ src/hooks/useUser.js
@@ -0,0 +1,13 @@
+import Meteor  from '@meteorrn/core'
+import { useState } from 'react'
+
+export const useUser = () => {
+  const [user, setUser] = useState()
+
+  Meteor.useTracker(computation => {
+    const current = Meteor.user()
+    if (user !== current) setUser(current)
+  })
+
+  return { user }
+}
