b9e3f0a608f64d186591e48ee69f2179a7b56e95
jankapunkt <jkuester@uni-bremen.de>
2023-01-05 11:49:16 +0100

    app: useConnection improved logging

--- src/lib/hooks/useConnection.js
+++ src/lib/hooks/useConnection.js
@@ -34,48 +34,14 @@ export const useConnection = () => {
   const status = Meteor.useTracker(() => Meteor.status())
 
   if (status.connected && !connected) {
-    log('set connnected')
+    log('set connected', status)
     setConnected(true)
   }
 
   if (connected && !status.connected) {
-    log('set connnected')
+    log('set disconnected', status)
     setConnected(false)
   }
-  // // we use separate functions as the handlers, so they get removed
-  // // on unmount, which happens on auto-reload and would cause errors
-  // // if not handled
-  // useEffect(() => {
-  //   const onError = (e) => setConnectionError(e)
-  //   Meteor.ddp.on('error', onError)
-  //
-  //   const onConnected = () => {
-  //     log('connected to', Config.backend.url)
-  //     if (connected !== true) setConnected(true)
-  //   }
-  //
-  //   Meteor.ddp.on('connected', onConnected)
-  //
-  //   // if the connection is lost, we not only switch the state
-  //   // but also force to reconnect to the server
-  //   const onDisconnected = () => {
-  //     console.debug('disconnected from', Config.backend.url)
-  //     Meteor.ddp.autoConnect = true
-  //     if (connected !== false) {
-  //       setConnected(false)
-  //     }
-  //     Meteor.reconnect()
-  //   }
-  //
-  //   Meteor.ddp.on('disconnected', onDisconnected)
-  //
-  //   // remove all of these listeners on unmount
-  //   return () => {
-  //     Meteor.ddp.off('error', onError)
-  //     Meteor.ddp.off('connected', onConnected)
-  //     Meteor.ddp.off('disconnected', onDisconnected)
-  //   }
-  // }, [])
 
   return { connected }
 }
