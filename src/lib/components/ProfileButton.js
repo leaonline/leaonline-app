8440762460b2f3aa8533509dec9b0172e7b12cdd
jankapunkt <jkuester@uni-bremen.de>
2023-01-10 16:50:22 +0100

    feature(app): ripple effect for profile button

--- src/lib/components/ProfileButton.js
+++ src/lib/components/ProfileButton.js
@@ -1,24 +1,32 @@
 import React from 'react'
-import { Vibration, View } from 'react-native'
+import { Pressable, Vibration, View } from 'react-native'
 import { Icon } from 'react-native-elements'
 import Colors from '../constants/Colors'
 import { useNavigation } from '@react-navigation/native'
+import nextFrame from 'next-frame'
 
 export const ProfileButton = props => {
   const navigation = useNavigation()
-  const toProfile = () => {
+  const toProfile = async () => {
+    await nextFrame()
     if (props.vibrate !== false) {
       Vibration.vibrate(100)
     }
     navigation.navigate(props.route)
   }
   return (
-    <View style={{ display: 'flex', marginLeft: 'auto' }}>
+    <Pressable android_ripple={rippleConfig}  onPress={toProfile} style={{ display: 'flex', marginLeft: 'auto' }}>
       <Icon
         name='user' type='font-awesome-5' color={Colors.gray} reverse
         style size={18}
-        onPress={toProfile}
       />
-    </View>
+    </Pressable>
   )
+}
+
+const rippleConfig = {
+  color: Colors.dark,
+  foreground: true,
+  borderless: true,
+  radius: 20
 }
