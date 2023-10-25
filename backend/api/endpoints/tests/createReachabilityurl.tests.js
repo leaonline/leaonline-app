d0a2ce9d040e08a27f24eb5eaf3d1aa3b5cbd4cf
jankapunkt <jkuester@uni-bremen.de>
2023-10-25 10:37:34 +0200

    fix(backend): reachbility url called after short timeout

--- backend/api/endpoints/tests/createReachabilityurl.tests.js
+++ backend/api/endpoints/tests/createReachabilityurl.tests.js
@@ -1,20 +1,11 @@
 /* eslint-env mocha */
-import { Meteor } from 'meteor/meteor'
 import { expect } from 'chai'
-import { HTTP } from 'meteor/jkuester:http'
 import { createReachabilityUrl } from '../createReachabilityUrl'
-import { asyncTimeout } from '../../utils/asyncTimeout'
 
 describe(createReachabilityUrl.name, () => {
-  it('creates a reachability url for a given path', async () => {
+  it('creates a reachability url for a given path', () => {
     const path = '/foo-bar-baz'
     createReachabilityUrl({ path })
-    await asyncTimeout(50)
-
-    const url = Meteor.absoluteUrl(path)
-    const response = HTTP.call('get', url)
-    expect(response.statusCode).to.equal(204)
-
     expect(() => createReachabilityUrl({ path }))
       .to.throw(`Path ${path} already taken!`)
   })
