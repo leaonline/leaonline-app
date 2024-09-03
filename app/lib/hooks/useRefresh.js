4c407da975bd3514c0d2279b24d39342bb3e1c93
jankapunkt <jkuester@uni-bremen.de>
2024-09-03 11:48:06 +0200

    docs: updated backend/app docs

--- app/lib/hooks/useRefresh.js
+++ app/lib/hooks/useRefresh.js
@@ -4,7 +4,7 @@ import { useCallback, useState } from 'react'
  * A little helper hook, that can be used to mediate between
  * {useDocs} and {BaseScreen} in order to implement a
  * page-refresh functionality.
- * @return {[number,(function(): void)|*]}
+ * @return {Array<number, function(): void>}
  */
 export const useRefresh = () => {
   const [reload, setReload] = useState(0)
