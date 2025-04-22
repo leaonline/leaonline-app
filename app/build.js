2a42cec248f0214752c96d07956d382da78663b9
jankapunkt <jkuester@uni-bremen.de>
2025-04-22 09:19:52 +0200

    buid(app): update local build system

--- app/build.js
+++ app/build.js
@@ -2,6 +2,7 @@ const fs = require('node:fs')
 const path = require('node:path')
 const { spawnSync } = require('node:child_process')
 
+const info = (...args) => console.info('[BUILD]', ...args)
 let BUILD_PROFILE = 'preview'
 let BUILD_LOCAL = false
 
@@ -15,6 +16,9 @@ process.argv
     }
   })
 
+info('use profile', BUILD_PROFILE)
+info('use local?', BUILD_LOCAL)
+
 const SETTINGS_PATH = path.join(process.cwd(), ((type) => {
   switch (type) {
     case 'staging':
@@ -25,18 +29,23 @@ const SETTINGS_PATH = path.join(process.cwd(), ((type) => {
       throw new Error(`Unknown build profile: ${type}`)
   }
 })(BUILD_PROFILE))
+info('settings path is', SETTINGS_PATH)
 
 const SETTINGS_DESTINATION = path.resolve('settings/settings.json')
+info('settings destination', SETTINGS_DESTINATION)
 const ORIGINAL_SETTINGS = fs.readFileSync(SETTINGS_DESTINATION)
+info('original settings', ORIGINAL_SETTINGS.length)
 const SETTINGS_SRC = fs.readFileSync(SETTINGS_PATH)
+info('settings src', SETTINGS_SRC.length)
 
 const recoverSettings = () => {
   fs.writeFileSync(SETTINGS_DESTINATION, ORIGINAL_SETTINGS)
 }
 
-const attempt = (fn) => {
+const attempt = (name, fn) => {
+  info('attempt', name)
   try {
-    fn()
+    return fn()
   }
   catch (e) {
     console.error(e)
@@ -45,11 +54,10 @@ const attempt = (fn) => {
   }
 }
 
-attempt(() => fs.writeFileSync(SETTINGS_DESTINATION, SETTINGS_SRC))
-attempt(() => {
+attempt('replace settings', () => fs.writeFileSync(SETTINGS_DESTINATION, SETTINGS_SRC))
+attempt('run build pipeline', () => {
   const args = ['eas', 'build', '--platform', 'android', '--profile', BUILD_PROFILE]
   if (BUILD_LOCAL) args.push('--local')
-
   spawnSync('npx', args, { stdio: 'inherit' })
   recoverSettings()
 })
