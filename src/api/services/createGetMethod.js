712756bd7fac1f853b6bfb4ca565ccd1c8b46b20
jankapunkt <jkuester@uni-bremen.de>
2026-08-20 10:38:01 +0200

    fix(server): create get method scoping error fixed

--- src/api/services/createGetMethod.js
+++ src/api/services/createGetMethod.js
@@ -1,15 +1,22 @@
 import { onServer } from '../../utils/archUtils'
 import { getCollection } from '../utils/getCollection'
+import { createLog } from "../../utils/createLog";
 
-export const createGetMethod = ({ context, run, backendOnly = true }) => {
+export const createGetMethod = ({ context, run, backendOnly = true, debug }) => {
+    const contextname = context.name
+    const methodName = `${contextname}.methods.get`
+    const prefix = `[${contextname}][${methodName}]:`
+    const _debug = debug ?? createLog({ name: context.name, level: 'debug' })
   return {
-    name: `${context.name}.methods.get`,
+    name: methodName,
     backend: backendOnly,
     schema: {
       _id: String
     },
     run: onServer(run || async function ({ _id }) {
-      return getCollection(context.name).findOneAsync({ _id })
+      const document = await getCollection(context.name).findOneAsync({ _id })
+        _debug(prefix, _id, `found=${!!document}`)
+      return document
     })
   }
 }
